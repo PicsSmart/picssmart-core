b012a3d4bd293f87f1b8aecdf18044de4e8edb8d
Banula Kumarage <63106638+BanulaKumarage@users.noreply.github.com>
2024-01-19 21:55:27 +0530

    Create a custom hook useApi to send api requests (#6)
    * Create custom hook useApi to send api requests
    
    * Aborted the previous requests in the axios service layer

---
+++ client/src/services/apiService/index.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+const options = {};
+
+const axiosProvider = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  ...options
+});
+
+const pendingRequests = {};
+
+const cancelPreviousRequests = (url, abort = false) => {
+  if (url && pendingRequests[url]) {
+    if (abort) {
+      console.log(`Aborting request: ${url}`);
+      pendingRequests[url].abort();
+    }
+    delete pendingRequests[url];
+  }
+};
+
+axiosProvider.interceptors.request.use(
+  (config) => {
+    console.log(pendingRequests);
+    if (config?.pendingRequests && config?.url && !config.signal) {
+      cancelPreviousRequests(config.url, true);
+      const abortController = new AbortController();
+      config.signal = abortController.signal;
+      pendingRequests[config.url] = abortController;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosProvider.interceptors.response.use(
+  (response) => {
+    cancelPreviousRequests(response.request.url);
+    return response;
+  },
+  (error) => {
+    cancelPreviousRequests(error.config?.url);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosProvider;

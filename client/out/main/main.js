cdbdb01ea2c842a826f85f2a932cf8aa5b5fc7a5
Akila Induranga Gamage <akila.99g@gmail.com>
2024-01-25 11:38:08 +0530

    Convert to electron without native integration (#10)

---
+++ client/out/main/main.js
@@ -0,0 +1,399 @@
+"use strict";
+const electron = require("electron");
+const require$$1 = require("path");
+const require$$0 = require("fs");
+const require$$2 = require("os");
+const require$$3 = require("crypto");
+var main$1 = { exports: {} };
+const name = "dotenv";
+const version$1 = "16.4.1";
+const description = "Loads environment variables from .env file";
+const main = "lib/main.js";
+const types = "lib/main.d.ts";
+const exports$1 = {
+  ".": {
+    types: "./lib/main.d.ts",
+    require: "./lib/main.js",
+    "default": "./lib/main.js"
+  },
+  "./config": "./config.js",
+  "./config.js": "./config.js",
+  "./lib/env-options": "./lib/env-options.js",
+  "./lib/env-options.js": "./lib/env-options.js",
+  "./lib/cli-options": "./lib/cli-options.js",
+  "./lib/cli-options.js": "./lib/cli-options.js",
+  "./package.json": "./package.json"
+};
+const scripts = {
+  "dts-check": "tsc --project tests/types/tsconfig.json",
+  lint: "standard",
+  "lint-readme": "standard-markdown",
+  pretest: "npm run lint && npm run dts-check",
+  test: "tap tests/*.js --100 -Rspec",
+  prerelease: "npm test",
+  release: "standard-version"
+};
+const repository = {
+  type: "git",
+  url: "git://github.com/motdotla/dotenv.git"
+};
+const funding = "https://github.com/motdotla/dotenv?sponsor=1";
+const keywords = [
+  "dotenv",
+  "env",
+  ".env",
+  "environment",
+  "variables",
+  "config",
+  "settings"
+];
+const readmeFilename = "README.md";
+const license = "BSD-2-Clause";
+const devDependencies = {
+  "@definitelytyped/dtslint": "^0.0.133",
+  "@types/node": "^18.11.3",
+  decache: "^4.6.1",
+  sinon: "^14.0.1",
+  standard: "^17.0.0",
+  "standard-markdown": "^7.1.0",
+  "standard-version": "^9.5.0",
+  tap: "^16.3.0",
+  tar: "^6.1.11",
+  typescript: "^4.8.4"
+};
+const engines = {
+  node: ">=12"
+};
+const browser = {
+  fs: false
+};
+const require$$4 = {
+  name,
+  version: version$1,
+  description,
+  main,
+  types,
+  exports: exports$1,
+  scripts,
+  repository,
+  funding,
+  keywords,
+  readmeFilename,
+  license,
+  devDependencies,
+  engines,
+  browser
+};
+const fs = require$$0;
+const path = require$$1;
+const os = require$$2;
+const crypto = require$$3;
+const packageJson = require$$4;
+const version = packageJson.version;
+const LINE = /(?:^|^)\s*(?:export\s+)?([\w.-]+)(?:\s*=\s*?|:\s+?)(\s*'(?:\\'|[^'])*'|\s*"(?:\\"|[^"])*"|\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*(?:#.*)?(?:$|$)/mg;
+function parse(src) {
+  const obj = {};
+  let lines = src.toString();
+  lines = lines.replace(/\r\n?/mg, "\n");
+  let match;
+  while ((match = LINE.exec(lines)) != null) {
+    const key = match[1];
+    let value = match[2] || "";
+    value = value.trim();
+    const maybeQuote = value[0];
+    value = value.replace(/^(['"`])([\s\S]*)\1$/mg, "$2");
+    if (maybeQuote === '"') {
+      value = value.replace(/\\n/g, "\n");
+      value = value.replace(/\\r/g, "\r");
+    }
+    obj[key] = value;
+  }
+  return obj;
+}
+function _parseVault(options2) {
+  const vaultPath = _vaultPath(options2);
+  const result = DotenvModule.configDotenv({ path: vaultPath });
+  if (!result.parsed) {
+    const err = new Error(`MISSING_DATA: Cannot parse ${vaultPath} for an unknown reason`);
+    err.code = "MISSING_DATA";
+    throw err;
+  }
+  const keys = _dotenvKey(options2).split(",");
+  const length = keys.length;
+  let decrypted;
+  for (let i = 0; i < length; i++) {
+    try {
+      const key = keys[i].trim();
+      const attrs = _instructions(result, key);
+      decrypted = DotenvModule.decrypt(attrs.ciphertext, attrs.key);
+      break;
+    } catch (error) {
+      if (i + 1 >= length) {
+        throw error;
+      }
+    }
+  }
+  return DotenvModule.parse(decrypted);
+}
+function _log(message) {
+  console.log(`[dotenv@${version}][INFO] ${message}`);
+}
+function _warn(message) {
+  console.log(`[dotenv@${version}][WARN] ${message}`);
+}
+function _debug(message) {
+  console.log(`[dotenv@${version}][DEBUG] ${message}`);
+}
+function _dotenvKey(options2) {
+  if (options2 && options2.DOTENV_KEY && options2.DOTENV_KEY.length > 0) {
+    return options2.DOTENV_KEY;
+  }
+  if (process.env.DOTENV_KEY && process.env.DOTENV_KEY.length > 0) {
+    return process.env.DOTENV_KEY;
+  }
+  return "";
+}
+function _instructions(result, dotenvKey) {
+  let uri;
+  try {
+    uri = new URL(dotenvKey);
+  } catch (error) {
+    if (error.code === "ERR_INVALID_URL") {
+      const err = new Error("INVALID_DOTENV_KEY: Wrong format. Must be in valid uri format like dotenv://:key_1234@dotenv.org/vault/.env.vault?environment=development");
+      err.code = "INVALID_DOTENV_KEY";
+      throw err;
+    }
+    throw error;
+  }
+  const key = uri.password;
+  if (!key) {
+    const err = new Error("INVALID_DOTENV_KEY: Missing key part");
+    err.code = "INVALID_DOTENV_KEY";
+    throw err;
+  }
+  const environment = uri.searchParams.get("environment");
+  if (!environment) {
+    const err = new Error("INVALID_DOTENV_KEY: Missing environment part");
+    err.code = "INVALID_DOTENV_KEY";
+    throw err;
+  }
+  const environmentKey = `DOTENV_VAULT_${environment.toUpperCase()}`;
+  const ciphertext = result.parsed[environmentKey];
+  if (!ciphertext) {
+    const err = new Error(`NOT_FOUND_DOTENV_ENVIRONMENT: Cannot locate environment ${environmentKey} in your .env.vault file.`);
+    err.code = "NOT_FOUND_DOTENV_ENVIRONMENT";
+    throw err;
+  }
+  return { ciphertext, key };
+}
+function _vaultPath(options2) {
+  let possibleVaultPath = null;
+  if (options2 && options2.path && options2.path.length > 0) {
+    if (Array.isArray(options2.path)) {
+      for (const filepath of options2.path) {
+        if (fs.existsSync(filepath)) {
+          possibleVaultPath = filepath.endsWith(".vault") ? filepath : `${filepath}.vault`;
+        }
+      }
+    } else {
+      possibleVaultPath = options2.path.endsWith(".vault") ? options2.path : `${options2.path}.vault`;
+    }
+  } else {
+    possibleVaultPath = path.resolve(process.cwd(), ".env.vault");
+  }
+  if (fs.existsSync(possibleVaultPath)) {
+    return possibleVaultPath;
+  }
+  return null;
+}
+function _resolveHome(envPath) {
+  return envPath[0] === "~" ? path.join(os.homedir(), envPath.slice(1)) : envPath;
+}
+function _configVault(options2) {
+  _log("Loading env from encrypted .env.vault");
+  const parsed = DotenvModule._parseVault(options2);
+  let processEnv = process.env;
+  if (options2 && options2.processEnv != null) {
+    processEnv = options2.processEnv;
+  }
+  DotenvModule.populate(processEnv, parsed, options2);
+  return { parsed };
+}
+function configDotenv(options2) {
+  let dotenvPath = path.resolve(process.cwd(), ".env");
+  let encoding = "utf8";
+  const debug = Boolean(options2 && options2.debug);
+  if (options2) {
+    if (options2.path != null) {
+      let envPath = options2.path;
+      if (Array.isArray(envPath)) {
+        for (const filepath of options2.path) {
+          if (fs.existsSync(filepath)) {
+            envPath = filepath;
+            break;
+          }
+        }
+      }
+      dotenvPath = _resolveHome(envPath);
+    }
+    if (options2.encoding != null) {
+      encoding = options2.encoding;
+    } else {
+      if (debug) {
+        _debug("No encoding is specified. UTF-8 is used by default");
+      }
+    }
+  }
+  try {
+    const parsed = DotenvModule.parse(fs.readFileSync(dotenvPath, { encoding }));
+    let processEnv = process.env;
+    if (options2 && options2.processEnv != null) {
+      processEnv = options2.processEnv;
+    }
+    DotenvModule.populate(processEnv, parsed, options2);
+    return { parsed };
+  } catch (e) {
+    if (debug) {
+      _debug(`Failed to load ${dotenvPath} ${e.message}`);
+    }
+    return { error: e };
+  }
+}
+function config(options2) {
+  if (_dotenvKey(options2).length === 0) {
+    return DotenvModule.configDotenv(options2);
+  }
+  const vaultPath = _vaultPath(options2);
+  if (!vaultPath) {
+    _warn(`You set DOTENV_KEY but you are missing a .env.vault file at ${vaultPath}. Did you forget to build it?`);
+    return DotenvModule.configDotenv(options2);
+  }
+  return DotenvModule._configVault(options2);
+}
+function decrypt(encrypted, keyStr) {
+  const key = Buffer.from(keyStr.slice(-64), "hex");
+  let ciphertext = Buffer.from(encrypted, "base64");
+  const nonce = ciphertext.subarray(0, 12);
+  const authTag = ciphertext.subarray(-16);
+  ciphertext = ciphertext.subarray(12, -16);
+  try {
+    const aesgcm = crypto.createDecipheriv("aes-256-gcm", key, nonce);
+    aesgcm.setAuthTag(authTag);
+    return `${aesgcm.update(ciphertext)}${aesgcm.final()}`;
+  } catch (error) {
+    const isRange = error instanceof RangeError;
+    const invalidKeyLength = error.message === "Invalid key length";
+    const decryptionFailed = error.message === "Unsupported state or unable to authenticate data";
+    if (isRange || invalidKeyLength) {
+      const err = new Error("INVALID_DOTENV_KEY: It must be 64 characters long (or more)");
+      err.code = "INVALID_DOTENV_KEY";
+      throw err;
+    } else if (decryptionFailed) {
+      const err = new Error("DECRYPTION_FAILED: Please check your DOTENV_KEY");
+      err.code = "DECRYPTION_FAILED";
+      throw err;
+    } else {
+      throw error;
+    }
+  }
+}
+function populate(processEnv, parsed, options2 = {}) {
+  const debug = Boolean(options2 && options2.debug);
+  const override = Boolean(options2 && options2.override);
+  if (typeof parsed !== "object") {
+    const err = new Error("OBJECT_REQUIRED: Please check the processEnv argument being passed to populate");
+    err.code = "OBJECT_REQUIRED";
+    throw err;
+  }
+  for (const key of Object.keys(parsed)) {
+    if (Object.prototype.hasOwnProperty.call(processEnv, key)) {
+      if (override === true) {
+        processEnv[key] = parsed[key];
+      }
+      if (debug) {
+        if (override === true) {
+          _debug(`"${key}" is already defined and WAS overwritten`);
+        } else {
+          _debug(`"${key}" is already defined and was NOT overwritten`);
+        }
+      }
+    } else {
+      processEnv[key] = parsed[key];
+    }
+  }
+}
+const DotenvModule = {
+  configDotenv,
+  _configVault,
+  _parseVault,
+  config,
+  decrypt,
+  parse,
+  populate
+};
+main$1.exports.configDotenv = DotenvModule.configDotenv;
+main$1.exports._configVault = DotenvModule._configVault;
+main$1.exports._parseVault = DotenvModule._parseVault;
+main$1.exports.config = DotenvModule.config;
+main$1.exports.decrypt = DotenvModule.decrypt;
+main$1.exports.parse = DotenvModule.parse;
+main$1.exports.populate = DotenvModule.populate;
+main$1.exports = DotenvModule;
+var mainExports = main$1.exports;
+const options = {};
+if (process.env.DOTENV_CONFIG_ENCODING != null) {
+  options.encoding = process.env.DOTENV_CONFIG_ENCODING;
+}
+if (process.env.DOTENV_CONFIG_PATH != null) {
+  options.path = process.env.DOTENV_CONFIG_PATH;
+}
+if (process.env.DOTENV_CONFIG_DEBUG != null) {
+  options.debug = process.env.DOTENV_CONFIG_DEBUG;
+}
+if (process.env.DOTENV_CONFIG_OVERRIDE != null) {
+  options.override = process.env.DOTENV_CONFIG_OVERRIDE;
+}
+if (process.env.DOTENV_CONFIG_DOTENV_KEY != null) {
+  options.DOTENV_KEY = process.env.DOTENV_CONFIG_DOTENV_KEY;
+}
+var envOptions = options;
+const re = /^dotenv_config_(encoding|path|debug|override|DOTENV_KEY)=(.+)$/;
+var cliOptions = function optionMatcher(args) {
+  return args.reduce(function(acc, cur) {
+    const matches = cur.match(re);
+    if (matches) {
+      acc[matches[1]] = matches[2];
+    }
+    return acc;
+  }, {});
+};
+(function() {
+  mainExports.config(
+    Object.assign(
+      {},
+      envOptions,
+      cliOptions(process.argv)
+    )
+  );
+})();
+let mainWindow;
+function createWindow() {
+  mainWindow = new electron.BrowserWindow({
+    autoHideMenuBar: true
+  });
+  mainWindow.loadURL("http://localhost:5173");
+  mainWindow.on("closed", () => mainWindow = null);
+}
+electron.app.whenReady().then(() => {
+  createWindow();
+});
+electron.app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    electron.app.quit();
+  }
+});
+electron.app.on("activate", () => {
+  if (mainWindow == null) {
+    createWindow();
+  }
+});

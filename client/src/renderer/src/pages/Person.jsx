cdb4a788071d3aa4ea971d174e0a8d32c9e51755
Hiruna Harankahadeniya <hiruna.19@cse.mrt.ac.lk>
2024-01-29 19:46:29 +0530

    Person page integration done

--- client/src/renderer/src/pages/Person.jsx
+++ client/src/renderer/src/pages/Person.jsx
@@ -1,18 +1,39 @@
 import { useParams } from 'react-router-dom';
 import { Box, CardMedia, Button } from '@mui/material/index';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { getFaceGroupImagesApi } from '../services/apiService/people';
+import ImageGallery from '../components/ImageGallery';
 
 const Person = () => {
   const { id } = useParams();
 
-  // dummy data
-  const person = {
-    id: id,
-    name: 'John Smith',
-    photoCount: 20,
-    favouriteCount: 5,
-    face: 'https://picsum.photos/id/1005/600/600'
-  };
+  const faces = useSelector((state) => state.faces.faces);
+  const media = useSelector((state) => state.media.media);
+  const [person, setPerson] = useState(faces.filter((face) => face._id === id)[0]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [photos, setPhotos] = useState([]); 
+
+  const getFaces = async () => {
+    try {
+      setLoading(true);
+      const { data } = await getFaceGroupImagesApi(id);
+      data.forEach(element => {
+        const img = media.filter((photo) => photo._id === element._id)[0]
+        setPhotos((prev)=>[...prev, img]);
+      });
+    } catch (exception) {
+      setError(exception);
+    } finally{
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => { 
+    getFaces();
+  } , []);
 
   const icon = (
     <BorderColorIcon
@@ -23,19 +44,18 @@ const Person = () => {
     />
   );
 
-  console.log(person);
   return (
     <div>
       <Box sx={{ width: '100%', height: '150px', display: 'flex', flexDirection: 'row' }}>
         <CardMedia
           sx={{ borderRadius: '50px', padding: '10px', width: '150px', height: '150px' }}
           component="img"
-          image={person.face}
-          alt={person.name}
+          image={`http://127.0.0.1:8000/thumbnail/${person.imageId}?top=${person.face.top}&right=${person.face.right}&bottom=${person.face.bottom}&left=${person.face.left}`}
+          alt={person._id}
         />
         <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginRight: '50px' }}>
           <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-            <h2>{person.name}</h2>
+            <h2>{person._id}</h2>
             <span style={{ display: 'flex', alignItems: 'center', marginRight: '10px' }}>{icon}</span>
           </Box>
           <Button variant="contained" color="error">
@@ -45,7 +65,7 @@ const Person = () => {
       </Box>
       <Box>
         <h1>Photos</h1>
-        {/* TODO: Need the photo card to populate here */}
+        <ImageGallery images={photos}/>
       </Box>
     </div>
   );

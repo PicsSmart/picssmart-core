b7199c7fdc3f758a38edc860305bf02f44357349
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-01-29 22:58:55 +0530

    Return additional parameters in text search response

--- server/indexing/image_text_search.py
+++ server/indexing/image_text_search.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import asyncio
 
 from PIL import Image
 import torch
@@ -7,6 +8,8 @@ from lavis.models import load_model_and_preprocess
 
 from server import conf
 from server.vectorDB import create_collection, upsert
+from server.db import media
+
 
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
@@ -15,26 +18,28 @@ from qdrant_client.http import models
 from multiprocessing import Process
 LOG = logging.getLogger(__name__)
 
-def vectorizing_images(task_dir):
+async def vectorizing_images(task_dir):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model, vis_processors, txt_processors = load_model_and_preprocess(name="blip_feature_extractor", model_type="base", 
                                                                       is_eval=True, device=device)
+    content = await media.get_media({}, {"_id": 1, "path":1, "albumIds":1, "name":1, "caption":1}, "name", 0, 1000)
     folder_path = os.path.join(task_dir, "data")
     payload = []
     data = []
-    for root, dirs, files in os.walk(folder_path):
-        for file_name in files:
-            if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
-                image_path = os.path.join(root, file_name)
-                record = {
-                    'path':image_path
-                }
-                raw_image = Image.open(image_path).convert("RGB")
-                image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
-                sample_img = {"image": image}
-                payload.append(record)
-                features_image = model.extract_features(sample_img, mode="image")
-                data.append(features_image.image_embeds_proj[:,0,:].cpu().numpy()[0])
+    for item in content:
+        image_path = os.path.join(folder_path, item["path"])
+        record = {
+            '_id': str(item["_id"]),
+            'path': item["path"],
+            'name': item["name"],
+            'caption': item["caption"] if "caption" in item else "",
+        }
+        raw_image = Image.open(image_path).convert("RGB")
+        image = vis_processors["eval"](raw_image).unsqueeze(0).to(device)
+        sample_img = {"image": image}
+        payload.append(record)
+        features_image = model.extract_features(sample_img, mode="image")
+        data.append(features_image.image_embeds_proj[:,0,:].cpu().numpy()[0])
     index = list(range(len(data)))
     return index, data, payload
 
@@ -49,7 +54,14 @@ def runner(task_dir, killer):
         LOG.debug(f"Collection {collection_name} already exists")
 
     LOG.debug(f"Starting vectorizing the images")
-    index, data, payload = vectorizing_images(task_dir)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    # Run the async function within the event loop
+    index, data, payload = loop.run_until_complete(vectorizing_images(task_dir))
+    
+    # Close the event loop
+    loop.close()
     upsert(collection_name, index, data, payload)
     LOG.debug(f"Vectorizing the images finished")
     

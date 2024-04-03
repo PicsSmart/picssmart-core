61baf052d1b7f4bd9659fedc6223fb803852c43a
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2024-04-03 23:24:42 +0530

    Migrated the client folder

--- client/out/preload/preload.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("electronAPI", {
-  "openFolder": () => electron.ipcRenderer.invoke("dialog:openFolder")
-});
